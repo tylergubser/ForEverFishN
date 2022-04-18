1aa216c036d60dc17245356e145d3d6d239723d6
tylergubser <tylergubser2200@gmail.com>
2022-04-17 20:14:43 -0500

    added user routes and login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.permit(:name, :email, :password)
+        params.permit(:username, :password_digest)
     end 
 
 end
